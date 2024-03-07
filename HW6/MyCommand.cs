57018359fbd51713d8819e93417fce2bec9b160b
DESKTOP-I94MI2F\user <iiipoizone@gmail.com>
2024-03-07 18:45:56 +0400

    Добавьте файлы проекта.

---
+++ HW6/MyCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW6
+{
+    class MyCommand : ICommand
+    {
+        private ICommand _next;
+        private string _name;
+
+        public MyCommand(ICommand next, string name)
+        {
+            _next = next;
+            _name = name;
+        }
+
+        public async Task Execute()
+        {
+            Console.WriteLine(_name);
+            if (_next != null)
+            {
+                await _next.Execute();
+            }
+        }
+
+
+    }
+}
---
+++ HW6/Program.cs
@@ -0,0 +1,21 @@
+﻿using HW6;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int size = 3;
+            // Создаем цепочку команд
+            ICommand c1 = new MyCommand(null,"Команда 1");
+            ICommand c3 = new MyCommand(c1, "Команда 3");
+            ICommand c2 = new MyCommand(c3, "Команда 2");
+
+
+            // Запускаем выполнение цепочки
+            await c2.Execute();
+        }
+    }
+}
